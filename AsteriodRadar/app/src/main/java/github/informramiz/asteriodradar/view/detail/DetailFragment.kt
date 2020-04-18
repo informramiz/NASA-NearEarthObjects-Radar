e0611a774352de8a2d85c1938f8b00906e750cd4
Ramiz Raja <informramiz@gmail.com>
2020-04-18 19:24:47 +0500

    feat: Create blank detail screen

---
+++ AsteriodRadar/app/src/main/java/github/informramiz/asteriodradar/view/detail/DetailFragment.kt
@@ -0,0 +1,33 @@
+package github.informramiz.asteriodradar.view.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import github.informramiz.asteriodradar.R
+import github.informramiz.asteriodradar.databinding.DetailFragmentBinding
+import github.informramiz.asteriodradar.view.base.BaseFragment
+
+class DetailFragment : BaseFragment() {
+
+    companion object {
+        fun newInstance() = DetailFragment()
+    }
+
+    private val viewModel: DetailViewModel by viewModelsUsingDaggerFactory()
+    private lateinit var viewBinding: DetailFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DetailFragmentBinding.inflate(layoutInflater, container, false)
+        return viewBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewBinding.lifecycleOwner = viewLifecycleOwner
+    }
+
+}
---
+++ AsteriodRadar/app/src/main/java/github/informramiz/asteriodradar/view/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package github.informramiz.asteriodradar.view.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
