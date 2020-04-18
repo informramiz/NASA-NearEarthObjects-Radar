fdc5d8c11ce90b505ef61af5d79de3ec0a0641cb
Ramiz Raja <informramiz@gmail.com>
2020-04-19 02:22:36 +0500

    feat: Show image of the day in UI

--- AsteriodRadar/app/src/main/java/github/informramiz/asteriodradar/view/overview/BindingAdapters.kt
+++ AsteriodRadar/app/src/main/java/github/informramiz/asteriodradar/view/overview/BindingAdapters.kt
@@ -1,8 +1,12 @@
 package github.informramiz.asteriodradar.view.overview
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import github.informramiz.asteriodradar.model.respository.domain.Asteroid
+import github.informramiz.asteriodradar.model.respository.domain.ImageOfTheDay
+import github.informramiz.asteriodradar.model.respository.utils.Response
+import github.informramiz.asteriodradar.view.commonextensions.setUrl
 
 /**
  * Created by Ramiz Raja on 19/04/2020
@@ -11,4 +15,13 @@ import github.informramiz.asteriodradar.model.respository.domain.Asteroid
 fun RecyclerView.setAsteroidsList(asteroids: List<Asteroid>?) {
     asteroids ?: return
     (adapter as AsteroidRecyclerAdapter).submitList(asteroids)
+}
+
+@BindingAdapter("imageOfTheDay")
+fun ImageView.setImageOfTheDay(imageOfTheDayResponse: Response<ImageOfTheDay>) {
+    when (imageOfTheDayResponse) {
+        is Response.SuccessResponse -> {
+            setUrl(imageOfTheDayResponse.data!!.url)
+        }
+    }
 }
--- AsteriodRadar/app/src/main/java/github/informramiz/asteriodradar/view/overview/OverviewViewModel.kt
+++ AsteriodRadar/app/src/main/java/github/informramiz/asteriodradar/view/overview/OverviewViewModel.kt
@@ -3,7 +3,11 @@ package github.informramiz.asteriodradar.view.overview
 import androidx.lifecycle.*
 import github.informramiz.asteriodradar.model.respository.AsteroidRepository
 import github.informramiz.asteriodradar.model.respository.domain.Asteroid
+import github.informramiz.asteriodradar.model.respository.domain.ImageOfTheDay
+import github.informramiz.asteriodradar.model.respository.utils.Response
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -14,6 +18,10 @@ class OverviewViewModel @Inject constructor(private val asteroidsRepository: Ast
         get() = _navigateToDetailsEvent
 
     val asteroids: LiveData<List<Asteroid>> = asteroidsRepository.getAsteroids().asLiveData(context = viewModelScope.coroutineContext)
+    val imageOfTheDay: LiveData<Response<ImageOfTheDay>> = liveData {
+        emit(Response.LoadingResponse())
+        emit(asteroidsRepository.getImageOfTheDay())
+    }
 
     init {
         viewModelScope.launch {
